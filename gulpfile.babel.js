af1f87bfd0fa1090eb763c58ddba89becefae273
agrinko <grinko.alexey@gmail.com>
2017-04-08 21:23:09 +0300

    Add CSS minification

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,10 +2,11 @@ import gulp from "gulp";
 import babel from "gulp-babel";
 import uglify from "gulp-uglify";
 import rename from "gulp-rename";
+import cleanCSS from "gulp-clean-css";
 
 gulp.task("default", ["build"]);
 
-gulp.task("build", ["babel"], () => {
+gulp.task("build", ["babel", "minify-css"], () => {
     return gulp.src([
             "src/*.js",
             "!src/*.min.js"
@@ -23,4 +24,22 @@ gulp.task("babel", () => {
     return gulp.src("src/*.es6")
         .pipe(babel())
         .pipe(gulp.dest("src"));
+});
+
+gulp.task("minify-css", () => {
+    return gulp.src([
+            "css/*.css",
+            "!css/*.min.css"
+        ])
+        .pipe(cleanCSS({
+            debug: true,
+            compatibility: "ie9"
+        }, (details) => {
+            console.log(details.name + ": " + details.stats.originalSize);
+            console.log(details.name + ": " + details.stats.minifiedSize);
+        }))
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest("css"));
 });
